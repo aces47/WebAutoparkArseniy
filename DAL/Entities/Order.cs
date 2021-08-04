c6adceac7f2ec0965a7650747cc30d6a2bce8494
aces47 <35996769+aces47@users.noreply.github.com>
2021-08-04 18:18:01 +0300

    Made Second week (#2)
    * Made FirstWeek
    
    * Fixed naming of ID
    Fixed Async Repositories
    Fixed Naming of constants
    Connected DB to App
    
    * Made Second Week Part
    
    * Fixed DI in Startup.cs

---
+++ DAL/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int VehicleId { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public ICollection<OrderElement> OrderElements { get; set; }
+    }
+}
---
+++ DAL/Repositories/OrderRepository.cs
@@ -0,0 +1,75 @@
+﻿using DAL.Entities;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace DAL.Repositories
+{
+    public class OrderRepository : BaseRepository, IRepository<Order>
+    {
+        private const string QueryCreate = "INSERT INTO Orders (VehicleId) VALUES(@VehicleId)";
+        private const string QueryDelete = "DELETE FROM Orders WHERE OrderId = @Id";
+        private const string QueryGetAll = "SELECT O.*,V.ModelName,V.StateNumber,OE.DetailCount,D.Name" +
+            "FROM Orders AS O" +
+            "LEFT JOIN OrderElements AS OE ON O.OrderId = OE.OrderId" +
+            "JOIN Vehicles AS V ON O.VehicleId = V.VehicleId" +
+            "LEFT JOIN Details AS D ON OE.DetailId = D.DetailId";
+        private const string QueryGetById = "SELECT O.*,V.ModelName,V.StateNumber,OE.DetailCount,D.Name" +
+            "FROM Orders AS O" +
+            "LEFT JOIN OrderElements AS OE ON O.OrderId = OE.OrderId" +
+            "JOIN Vehicles AS V ON O.VehicleId = V.VehicleId" +
+            "LEFT JOIN Details AS D ON OE.DetailId = D.DetailId" +
+            "WHERE OrderId = @Id";
+        private const string QueryUpdate = "UPDATE Orders SET VehicleId = @VehicleId WHERE OrderId = @Id";
+
+        public OrderRepository(string connectionString) : base(connectionString) { }
+
+        public async Task Create(Order item) => await connection.ExecuteAsync(QueryCreate, item);
+
+        public async Task Delete(int id) => await connection.ExecuteAsync(QueryDelete, id);
+
+        public async Task<IEnumerable<Order>> GetAll()
+        {
+            var orderElements = new List<OrderElement>();
+
+            return await connection.QueryAsync<Order, Vehicle, OrderElement, Detail, Order>
+            (QueryGetAll, (order, vehicle, orderElement, detail) =>
+            {
+                order.Vehicle = vehicle;
+                orderElement.Part = detail;
+                orderElements.Add(orderElement);
+
+                return order;
+            },
+            splitOn: "VId,OEId,SPId"
+            );
+        }
+
+
+        public async Task<Order> GetById(int id)
+        {
+            var orderElements = new List<OrderElement>();
+
+            var collection = await connection.QueryAsync<Order, Vehicle, OrderElement, Detail, Order>
+            (QueryGetAll, (order, vehicle, orderElement, detail) =>
+            {
+                order.Vehicle = vehicle;
+                orderElement.Part = detail;
+                orderElements.Add(orderElement);
+
+                return order;
+            },
+            splitOn: "VId,OEId,SPId",
+            param: new { id }
+            );
+
+            return collection.First();
+        }
+
+        public async Task Update(Order item) => await connection.ExecuteAsync(QueryUpdate, item);
+    }
+}
