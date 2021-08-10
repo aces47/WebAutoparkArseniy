722be4abeb6d1fe5ccd33c979c01860393bbd3b2
aces47 <ars.stank@gmail.com>
2021-08-10 10:41:56 +0300

    Add some controller and views for Vehicle entity

---
+++ WebAutopark/Controllers/VehicleController.cs
@@ -0,0 +1,77 @@
+﻿using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAutopark.Controllers
+{
+    public class VehicleController : Controller
+    {
+        private readonly IRepository<Vehicle> _vehicleRepository;
+
+        public VehicleController(IRepository<Vehicle> vehicleRepository)
+        {
+            _vehicleRepository = vehicleRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var vehicles = (await _vehicleRepository.GetAll()).OrderBy(e => e.VehicleId);
+
+            return View(vehicles);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Details(int id)
+        {
+            var vehicle = await _vehicleRepository.GetById(id);
+
+            return View(vehicle);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Vehicle vehicle)
+        {
+            await _vehicleRepository.Create(vehicle);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var vehicle = await _vehicleRepository.GetById(id);
+
+            return View(vehicle);
+        }
+
+        [HttpPut]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(Vehicle vehicle)
+        {
+            await _vehicleRepository.Update(vehicle);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpDelete]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _vehicleRepository.Delete(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
