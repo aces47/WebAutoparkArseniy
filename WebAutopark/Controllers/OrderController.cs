66c6e1915f30c9e0443894f4402eeee427232560
aces47 <ars.stank@gmail.com>
2021-08-24 04:45:12 +0300

    fixed some bugs with sorting

---
+++ WebAutopark/Controllers/OrderController.cs
@@ -0,0 +1,40 @@
+﻿using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebAutopark.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderController(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var orders = await _orderRepository.GetAll();
+            return View(orders);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var order = await _orderRepository.GetById(id);
+
+            return View(order);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Order item)
+        {
+            Order order = await _orderRepository.CreateReturn(item);
+            
+            return RedirectToAction("Create", "OrderElement", order);
+        }
+    }
+}
---
+++ WebAutopark/Controllers/OrderElementController.cs
@@ -0,0 +1,47 @@
+﻿using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAutopark.Controllers
+{
+    public class OrderElementController : Controller
+    {
+        private readonly IRepository<OrderElement> _orderElementRepository;
+        private readonly IRepository<Detail> _detailRepository;
+
+        public OrderElementController(IRepository<OrderElement> orderElementRepository,
+            IRepository<Detail> detailRepository)
+        {
+            _orderElementRepository = orderElementRepository;
+            _detailRepository = detailRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create(Order order)
+        {
+            var details = new SelectList(await _detailRepository.GetAll(), "DetailId", "Name");
+            ViewBag.Details = details;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(OrderElement orderElement)
+        {
+            if(ModelState.IsValid)
+            {
+                await _orderElementRepository.Create(orderElement);
+
+                return RedirectToAction("Details", "Order", new { id = orderElement.OrderId });
+            }
+
+            return View();
+        }
+    }
+}
